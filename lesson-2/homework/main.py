15561c84482d10af98f32c57b8c9e0beff7899ee
Dildora <79807014+dildora69@users.noreply.github.com>
2025-03-28 14:54:20 +0500

    28/3/2025

--- lesson-2/homework/main.py
+++ lesson-2/homework/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 # 1. Age Calculator
 # Write a Python program to ask for a user's name and year of birth, then calculate and display their age.
 
@@ -42,5 +45,56 @@
 
 # 6. Count Vowels
 # Write a Python program that counts the number of vowels in a given string.
+
+# str1 = input('Please enter a word to count its VOWELS: ')
+# count = 0
+# for i in str1.lower():
+#
+#     if i == 'a' or i == 'y' or i == 'i' or i == 'o' or i == 'e' or i == 'u':
+#         count += 1
+#     else:
+#         pass
+#
+# print(f'The number of Vowels in the sentence: {count}')
+
+#
+# 7. Find Maximum Value
+# Write a Python program that takes a list of numbers as input and prints the maximum value
+
+# num = input('Please enter numbers with comas: ')
+# new_ls = num.split(',')
+# max_num = 0
+# for i in range(0, len(new_ls)-1):
+#     if max_num < int(new_ls[i]):
+#         max_num = int(new_ls[i])
+#     else:
+#         pass
+# print(max_num)
+
+
+# 8. Check Palindrome
+# Write a Python program that checks if a given word is a palindrome (reads the same forward and backward).
+
+# pol = input('Please enter a word to check weather it is Palindrome or not: ')
+# if pol[::-1].lower() == pol.lower():
+#     print(f'The word {pol} is a Palindrome: {pol[::-1]}')
+# else:
+#     print(f'The word {pol} is not Palindrome: {pol[::-1]} ')
+
+
+# 9. Extract Email Domain
+# Write a Python program that extracts and prints the domain from an email address provided by the user.
+
+
+# email = input('Please enter your email: ')
+# try:
+#     email_domain = email.split('@')
+#     print(email_domain[1])
+# except:
+#     print('Please enter right format of email: example@email.com')
+
+
+# 10. Generate Random Password
+# Write a Python program to generate a random password containing letters, digits, and special characters.
 
 
