37526942bac66d05a9557a6dfc3db50cbbbdfd3e
Dildora <79807014+dildora69@users.noreply.github.com>
2025-04-09 05:12:21 +0500

    ex_5

--- lesson-5/homework/main.py
+++ lesson-5/homework/main.py
@@ -1,1 +1,39 @@
+# Homework:
+# 1.
+# def is_leap(year): """ Determines whether a given year is a leap year.
+
+#
+# year = int(input('Please Enter a year to check weather it is LEAP year or not:  '))
+#
+# if year % 4 == 0 or (year % 4 == 0 and (year % 100 == 0 and year % 400 == 0)):
+#     print('Yes, It is a LEAP year')
+# else:
+#     print('No not a LEAP year')
+#
+# # 2. Conditional Statements Exercise
+#
+# number = int(input('Please Enter a number: '))
+#
+# if number % 2 != 0:
+#     print('Weird')
+# elif number % 2 == 0 and (2 <= number <= 5):
+#     print('Not Weird')
+# elif number % 2 == 0 and (6 <= number <= 20):
+#     print('Weird')
+# else:
+#     print('Weirddd')
+
+# 3. Given two integer numbers a and b. Find even numbers between this numbers. a and b are inclusive. Don't use loop.
+# Give two solutions.
+#
+# Solution 1 with if-else statement.
+
+a = 6
+b = 15
+
+
+
+# Solution 2 without if-else statement.
+
+
 
